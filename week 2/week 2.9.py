0b80a5cf9c48675211495f4e67bf5ac17c269dc8
saran-raj-247 <231001184@rajalakshmi.edu.in>
2024-06-15 19:05:57 +0530

    Create week 2.9.py

---
+++ week 2/week 2.9.py
@@ -0,0 +1,7 @@
+a=int(input())
+a1=a+a*4/100
+a2=a1+a1*4/100
+a3=a2+a2*4/100
+print("Balance as of end of Year 1: $%.2f."%a1)
+print("Balance as of end of Year 2: $%.2f."%a2)
+print("Balance as of end of Year 3: $%.2f."%a3)
